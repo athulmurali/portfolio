637554372a2d8cc1fe1921abb27c3aacc86186e0
Athul Muralidharan <athulmurali@gmail.com>
2018-06-12 18:09:48 -0400

    FullPageReact issue

--- src/containers/FullPageReact.js
+++ src/containers/FullPageReact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Fullpage, HorizontalSlider, Slide } from 'fullpage-react/lib/index';
+import { Fullpage, HorizontalSlider, Slide } from 'fullpage-react/FullpageReact';
 const { changeFullpageSlide, changeHorizontalSlide } = Fullpage;
 
 
