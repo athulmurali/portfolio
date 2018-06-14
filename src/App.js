0cc90b34110240e19132224b4185cdb8f189aa24
Athul Muralidharan <athulmurali@gmail.com>
2018-06-14 03:27:11 -0400

    latest update

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
+    return <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit
+            <code>src/App.js</code> and save to reload.
         </p>
+        <h1> Cya soon! - Athul</h1>
       </div>
-    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 registerServiceWorker();
