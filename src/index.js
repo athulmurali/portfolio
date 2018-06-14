736af06db6ef75f707fcf580d5f040af06197c1a
Athul Muralidharan <athulmurali@gmail.com>
2018-06-14 02:55:26 -0400

    React installation fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import FullPageReact from "./containers/FullPageReact";
+import App from "./App";
 
-ReactDOM.render(<FullPageReact />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
